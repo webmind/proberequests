2f2f92fe9707f2b938ac283b8917d8b574672197
webmind <webmind@puscii.nl>
2017-10-13 16:06:05 +0200

    added OSC msgs

--- collector.pl
+++ collector.pl
@@ -3,17 +3,30 @@ use strict;
 use JSON;
 use Getopt::Long;
 use Redis;
+use Protocol::OSC;
+use IO::Socket::INET;
+
 
 my %h;
 $h{DEBUG} = 0;
 $h{tsharkPath} = '/usr/bin/tshark';
 $h{redis} = '127.0.0.1:6379';
 $h{redisname} = 'probereqdb';
-GetOptions (\%h, 'device=s', 'redis=s', 'tsharkPath=s', 'DEBUG');
+$h{OSCPort} = 5555;
+$h{OSCPeer} = '';
+GetOptions (\%h, 'device=s', 'redis=s', 'tsharkPath=s', 'OSCPort=i', 'OSCPeer=s', 'DEBUG');
 
 if((!defined $h{device})) {
     print STDERR "Usage: $0 --device=<wireless device> <--redis=hostname:port> <--redisname=dbname> <--tsharkPath=/path/to/tshark> <--DEBUG>\n\n";
     exit 1;
+}
+
+## If we have an OSCPeer host, we start using OSC.
+my $udp;
+my $osc;
+if($h{OSCPeer}) {
+    $udp = IO::Socket::INET->new( PeerAddr => $h{OSCPeer}, PeerPort => $h{OSCPort}, Proto => 'udp', Type => SOCK_DGRAM) || die "Cannot connect via UDP on $h{OSCPeer}:$h{OSCPort} ($!)\n";
+    $osc = Protocol::OSC->new;
 }
 
 my $redis = Redis->new(server => $h{redis},
@@ -31,16 +44,34 @@ while (my $line = <$tshark>) {
             my $macAddress = $1;
             my $SSID = $2;
             my $struct = readredis($redis, $SSID);
-            if(!defined($struct->{lastseen}) or 
+            if(!defined($struct->{lastSeen}) or 
                !defined($struct->{macs}->{$macAddress}) or 
-               ($struct->{lastseen} - time) >= 1) {
+               ($struct->{lastSeen} - time) >= 1) {
+                print "Spotted $SSID from $macAddress\n" if($h{DEBUG});
 
+                ## Send OSC first if needed
+                if($udp && $osc) {
+                    ## Send SSID
+                    my @ssidblob = map({ord} split(//, $SSID));
+                    my $ssidblobtypes = join('', map({'i'} split(//, $SSID)));
+                    $udp->send($osc->message('/network', $ssidblobtypes, @ssidblob));
+                    print "Send network over OSC($h{OSCPeer}:$h{OSCPort}): $SSID\n"
+                        if($h{DEBUG});
+                    
+                    ## Send mac
+                    my @macblob = map({hex} split(/:/, $macAddress));
+                    my $macblobtypes = join('', map({'i'} split(/:/, $macAddress)));
+                    $udp->send($osc->message('/mac', $macblobtypes, @macblob));
+                    print "Send mac over OSC($h{OSCPeer}:$h{OSCPort}): $macAddress\n"
+                        if($h{DEBUG});
+                }
+
+                $struct->{firstSeen} = time if(!defined($struct->{lastseen}));
                 $struct->{macs}->{$macAddress}++;
                 $struct->{name} = $SSID;
                 $struct->{count}++;
                 $struct->{lastSeen} = time;
                 writeredis($redis, $SSID, $struct);
-                print "Spotted $SSID from $macAddress\n" if($h{DEBUG});
             } else {
                 print "Double of $SSID?\n" if($h{DEBUG});
             }
